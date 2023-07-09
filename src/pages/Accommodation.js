65956ba28818ef42e37e2f5be7263e85afd148df
Gael F <gaelfoini@gmail.com>
2023-07-09 10:24:21 +0200

    responsive done, need more comments and variables for maintenance

--- src/pages/Accommodation.js
+++ src/pages/Accommodation.js
@@ -55,16 +55,13 @@ const Accommodation = () => {
           <Rating rating={accommodation.rating} />
         </div>
       </div>
-      <div
-        className="expander-container"
-        style={{ justifyContent: "space-between" }}
-      >
+      <div className="expander-container-accommodation">
         <Expander
           title="Description"
           content="Vous serez à 50m du canal Saint-martin où vous pourrez pique-niquer l'été et 
-        à côté de nombreux bars et restaurants. Au cœur de Paris avec 5 lignes de métro et de 
-        nombreux bus. Logement parfait pour les voyageurs en solo et les voyageurs d'affaires. 
-        Vous êtes à1 station de la gare de l'est (7 minutes à pied). "
+          à côté de nombreux bars et restaurants. Au cœur de Paris avec 5 lignes de métro et de 
+          nombreux bus. Logement parfait pour les voyageurs en solo et les voyageurs d'affaires. 
+          Vous êtes à1 station de la gare de l'est (7 minutes à pied). "
           containerWidth="582px"
           titleFontStyle="18px"
           paragraphFontStyle="18px"
