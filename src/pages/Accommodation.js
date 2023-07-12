2ccb872ca6814eb69988f1f5af933be8d82f1f04
Gael F <gaelfoini@gmail.com>
2023-07-12 22:33:49 +0200

    dynamic content on Accommodation page

--- src/pages/Accommodation.js
+++ src/pages/Accommodation.js
@@ -58,10 +58,7 @@ const Accommodation = () => {
       <div className="expander-container-accommodation">
         <Expander
           title="Description"
-          content="Vous serez à 50m du canal Saint-martin où vous pourrez pique-niquer l'été et 
-          à côté de nombreux bars et restaurants. Au cœur de Paris avec 5 lignes de métro et de 
-          nombreux bus. Logement parfait pour les voyageurs en solo et les voyageurs d'affaires. 
-          Vous êtes à1 station de la gare de l'est (7 minutes à pied). "
+          content={accommodation.description}
           containerWidth="582px"
           titleFontStyle="18px"
           paragraphFontStyle="18px"
