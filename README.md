41a9bee4120ee444d8c3bc88dbef07df4a920c7e
Gael F <gaelfoini@gmail.com>
2023-07-19 18:16:16 +0200

    add comment to accommodation page, some modifications on readme, need to comment other pages and components

--- README.md
+++ README.md
@@ -7,7 +7,7 @@
 </div>
 
 <p align="center">
-Projet Front-end React pour OpenClassrooms
+Projet Front-end React JS pour OpenClassrooms
 <br/>
 Par Gaël Foini
 <br/>
@@ -15,9 +15,9 @@ Par Gaël Foini
 
 ## À propos
 
-Le projet Kasa, est une plateforme de location d'appartements entre particuliers, développée par Gaël Foini dans le cadre d'un projet Front-end pour OpenClassrooms.
+Le projet Kasa est une plateforme de location d'appartements entre particuliers, développée par Gaël Foini dans le cadre d'un projet Front-end pour OpenClassrooms.
 
-Maquette : https://www.figma.com/file/bAnXDNqRKCRRP8mY2gcb5p/ARCHIVE-UI-Design-Kasa-FR?type=design&node-id=0-1&mode=design&t=uNUG5zH9Ppj6kt4Q-0
+-[Maquette Figma](https://www.figma.com/file/bAnXDNqRKCRRP8mY2gcb5p/ARCHIVE-UI-Design-Kasa-FR?type=design&node-id=0-1&mode=design&t=uNUG5zH9Ppj6kt4Q-0)
 
 ### Ce projet consiste à intégrer une maquette pour le site Kasa en utilisant les technologies suivantes :
 
@@ -33,33 +33,33 @@ Le site Kasa propose les fonctionnalités suivantes :
 
 - Affichage des annonces de logements à partir des données fournies dans un fichier JSON.
 - Navigation entre les différentes pages en utilisant React Router.
+- Redirection vers une page Erreur 404 en cas d'URL non valide.
 - Gestion d'une galerie d'images pour chaque logement avec des fonctionnalités spécifiques.
-- Utilisation des Collapse pour afficher/dissimuler des informations supplémentaires sur les logements.
-- Respect des contraintes techniques pour un code de qualité.
+- Utilisation de composants réutilisables.
 
 ## Mise en place
 
-Pour pouvoir utiliser le site et accéder à toutes les fonctionnalités que propose Kasa, assurez-vous d'avoir les composants nécessaire installé :
+Pour pouvoir utiliser le site et accéder à toutes les fonctionnalités que propose Kasa, assurez-vous d'avoir les composants nécessaires installés :
 
 - [Node.js](https://nodejs.org/en/) (version recommandée : 18.16.0)
 
 ## Installation
 
-Clonez le projet à partir de https://github.com/fgael/GaelFoini_11_06072023
-
-à partir du dossier cloné :
+Cloné le projet à partir de https://github.com/fgael/GaelFoini_11_06072023
 
 ```
+git clone https://github.com/fgael/GaelFoini_11_06072023.git
+cd GaelFoini_11_06072023/
 npm i
 ```
 
-### Démarrage frontend
+### Démarrage du site kasa
 
 ```
 npm run start
 ```
 
-## Contacts
+## Contact
 
 Gaël Foini - gaelfoinidev@gmail.com
 
--- src/pages/Accommodation.js
+++ src/pages/Accommodation.js
@@ -7,40 +7,56 @@ import Rating from "../components/Rating";
 import Expander from "../components/Expander";
 
 const Accommodation = () => {
+  // récupération de l'id depuis l'URL à l'aide de useParams
   const { id } = useParams();
+
+  // utilisation du hook useNavigate fourni par react-router-dom
   const navigate = useNavigate();
+
+  // définition des variables d'état à l'aide du hook useState
   const [accommodation, setAccommodation] = useState({});
   const [loading, setLoading] = useState(true);
 
+  // fonction asynchrone pour récupérer les données d'hébergement par l'id avec useEffect
   useEffect(() => {
     const fetchData = async () => {
       try {
+        // récupération des données d'hébergement par l'id
         const data = await fetchAccommodationById(id);
+        // vérification si l'id correspond à un élément dans les données
         if (data === undefined) {
+          // redirection vers la page d'erreur 404 en cas d'id non valide
           navigate("/Error404");
         } else {
+          // mise à jour des états avec les nouvelles données
           setAccommodation(data);
           setLoading(false);
         }
       } catch (error) {
+        // gestion des erreurs lors de la récupération des données
         console.error("Error fetching accommodation data:", error);
       }
     };
 
+    // appel de la fonction asynchrone fetchData au montage de la page (et lorsque l'id change)
     fetchData();
   }, [id, navigate]);
 
+  // si loading est true, cela signifie que les données ne sont pas encore chargées
   if (loading) {
     return null;
   }
 
+  // si loading est false, cela signifie que les données ont été chargées avec succès
   return (
     <div className="accommodation-container">
+      {/* appel du composant carrousel pour afficher les images de l'hébergement */}
       <Carrousel accommodation={accommodation} />
       <div className="main-top-container">
         <div>
           <h2>{accommodation.title}</h2>
           <h4 className="location">{accommodation.location}</h4>
+          {/* appel du composant tag pour afficher les tags de l'hébergement */}
           <Tag tagList={accommodation.tags} />
         </div>
         <div className="main-top-container-right">
@@ -52,10 +68,12 @@ const Accommodation = () => {
               alt="host"
             ></img>
           </div>
+          {/* appel du composant rating pour afficher le rating de l'hébergement */}
           <Rating rating={accommodation.rating} />
         </div>
       </div>
       <div className="expander-container-accommodation">
+        {/* appel du composant expander avec des props fourni pour personnaliser le composant */}
         <Expander
           title="Description"
           content={accommodation.description}
@@ -66,6 +84,7 @@ const Accommodation = () => {
         <Expander
           title="Equipements"
           content={accommodation.equipments.map((equipment, index) => (
+            // creation d'un element DOM avec React.Fragment sans creer de noeud supplementaire
             <React.Fragment key={index}>
               {equipment}
               <br />
